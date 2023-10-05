da01e2f723415981965fce55604e99f557f4419f
runzac <91915526+alerunza@users.noreply.github.com>
2023-10-05 16:41:36 +0200

    add popuop statistiche to profile
    Co-Authored-By: imgaandy <126667123+imgaandy@users.noreply.github.com>

--- app/src/main/java/com/example/hangman/ProfileFragment.java
+++ app/src/main/java/com/example/hangman/ProfileFragment.java
@@ -1,14 +1,15 @@
 package com.example.hangman;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,55 +25,19 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    FirebaseAuth auth;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Altri codici di inizializzazione...
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getActivity().getWindow();
@@ -87,9 +51,7 @@ public class ProfileFragment extends Fragment {
             systemUiVisibilityFlags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             decorView.setSystemUiVisibility(systemUiVisibilityFlags);
         }
-
     }
-
 
     private void getUserEmailAndName(final OnSuccessListener<String> emailListener, final OnSuccessListener<String> nameListener, final OnFailureListener failureListener) {
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -131,7 +93,13 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         Button logoutActivityButton = view.findViewById(R.id.logOutIcon);
         auth = FirebaseAuth.getInstance();
-
+        AppCompatButton popupbutton = view.findViewById(R.id.popupbtn);
+        popupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowPopup(view); // Chiama il metodo ShowPopup quando il pulsante viene premuto
+            }
+        });
         final TextView emailTextView = view.findViewById(R.id.email);
         final TextView nameTextView = view.findViewById(R.id.username);
 
@@ -155,13 +123,21 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
-    /*
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
 
-     */
+    public void ShowPopup(View view) {
+        Dialog myDialog = new Dialog(requireContext());
+        Button btn;
+        myDialog.setContentView(R.layout.popup);
+        btn = (Button) myDialog.findViewById(R.id.chiudipopup);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
 }
