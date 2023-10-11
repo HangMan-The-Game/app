7d42467bf2e4f25a9937a5d3e14f41c7c078f3d1
runzac <91915526+alerunza@users.noreply.github.com>
2023-10-11 20:49:28 +0200

    fix UI leaderboard

--- app/src/main/java/com/example/hangman/RankingAdapter.java
+++ app/src/main/java/com/example/hangman/RankingAdapter.java
@@ -34,31 +34,10 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.RankingV
 
         if (player.getPoints() > 4) {
             holder.positionTextView.setText(String.valueOf(position + 1));
-            holder.nameTextView.setText(player.getName() + " - " + String.valueOf(player.getPoints()));
-            //holder.scoreTextView.setText(String.valueOf(player.getPoints()));
+            holder.nameTextView.setText(player.getName());
+            holder.scoreTextView.setText(String.valueOf(player.getPoints()));
 
             Context context = holder.itemView.getContext();
-
-            if (position < 3) {
-                int color;
-                switch (position) {
-                    case 0:
-                        color = ContextCompat.getColor(context, R.color.gold);
-                        break;
-                    case 1:
-                        color = ContextCompat.getColor(context, R.color.silver);
-                        break;
-                    case 2:
-                        color = ContextCompat.getColor(context, R.color.bronze);
-                        break;
-                    default:
-                        color = ContextCompat.getColor(context, R.color.default_color);
-                        break;
-                }
-                holder.itemView.setBackgroundColor(color);
-            } else {
-                holder.itemView.setBackgroundColor(ContextCompat.getColor(context, R.color.default_color));
-            }
         } else {
             holder.itemView.setVisibility(View.GONE);
         }
@@ -80,14 +59,14 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.RankingV
     public class RankingViewHolder extends RecyclerView.ViewHolder {
         public TextView positionTextView;
         public TextView nameTextView;
-        //public TextView scoreTextView;
+        public TextView scoreTextView;
 
         public RankingViewHolder(@NonNull View itemView) {
             super(itemView);
 
             positionTextView = itemView.findViewById(R.id.textPosition);
             nameTextView = itemView.findViewById(R.id.textName);
-            //scoreTextView = itemView.findViewById(R.id.textScore);
+            scoreTextView = itemView.findViewById(R.id.textScore);
         }
     }
 
